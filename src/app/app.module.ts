52ca79a0c957823d62277c5db5efd44164685b35
edu0acdc <eduardo.salvadinha@outlook.pt>
2021-03-09 18:33:34 +0000

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
-import { CartService } from './cart.service';
-import { CartComponent } from './cart/cart.component';
+import { CartService } from "./cart.service";
+import { CartComponent } from "./cart/cart.component";
 
 @NgModule({
   imports: [
@@ -17,8 +17,8 @@ import { CartComponent } from './cart/cart.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
-      { path: "products/:productId", component: ProductDetailsComponent }
-      { path: "cart", component : CartComponent}
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent }
     ])
   ],
   declarations: [
@@ -27,7 +27,6 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponentComponent,
     CartComponent
   ],
   bootstrap: [AppComponent],
